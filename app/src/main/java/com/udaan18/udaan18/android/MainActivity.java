e6a0b273fd0ff36214b86d6b46646bd67220e50f
Abhi <abhiakbari023@gmail.com>
2018-02-26 02:26:12 -0800

    Initial commit.

--- app/src/main/java/com/udaan18/udaan18/android/MainActivity.java
+++ app/src/main/java/com/udaan18/udaan18/android/MainActivity.java
@@ -2,16 +2,63 @@ package com.udaan18.udaan18.android;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * Created by abhishek on 2/25/2018.
  */
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = (TabLayout) findViewById(R.id.tab);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.camera));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.camera));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.camera));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.camera));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.camera));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(viewPagerAdapter);
+        //        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+//        viewPager.removeOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        // viewPager.clearOnPageChangeListeners();
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
