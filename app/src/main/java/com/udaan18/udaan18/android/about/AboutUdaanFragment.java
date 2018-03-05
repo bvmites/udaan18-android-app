f5ac0664fe7d47ab990f523cc2a6b3ef9eb8d0c5
pranavgajera <pranavgajera1998@gmail.com>
2018-03-05 21:40:05 +0530

    about us UI.

--- app/src/main/java/com/udaan18/udaan18/android/about/AboutUdaanFragment.java
+++ app/src/main/java/com/udaan18/udaan18/android/about/AboutUdaanFragment.java
@@ -1,6 +1,5 @@
 package com.udaan18.udaan18.android.about;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.udaan18.udaan18.android.R;
-import com.udaan18.udaan18.android.databinding.FragmentAboutUdaanBinding;
 
 /**
  * Creator: Varun Barad
@@ -17,7 +15,7 @@ import com.udaan18.udaan18.android.databinding.FragmentAboutUdaanBinding;
  * Project: udaan18-android-app
  */
 public class AboutUdaanFragment extends Fragment {
-  private FragmentAboutUdaanBinding dataBinding;
+    View rootView;
   
   public static AboutUdaanFragment newInstance() {
     AboutUdaanFragment fragment = new AboutUdaanFragment();
@@ -27,7 +25,7 @@ public class AboutUdaanFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    this.dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_about_udaan, container, false);
-    return this.dataBinding.getRoot();
+      rootView = inflater.inflate(R.layout.fragment_about_udaan, container, false);
+      return rootView;
   }
 }
